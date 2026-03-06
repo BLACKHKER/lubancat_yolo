0217fd0af3ea2f81e45109fc8be5a5f95379c7ed
Zhao <bhkdos@gmail.com>
2026-03-06 15:44:11 +0800

    [REFACTOR] file tree update, remove device_id

---
+++ yolov5/cpp/fastrun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+eval "~/Development/VisualStudioWorkspace/alubancat_yolo/yolov5/cpp/build-linux.sh -t rk3588"
+eval "cp ~/Desktop/yolov5s_relu.rknn ~/Development/VisualStudioWorkspace/alubancat_yolo/yolov5/cpp/install/rk3588_linux/"
+
