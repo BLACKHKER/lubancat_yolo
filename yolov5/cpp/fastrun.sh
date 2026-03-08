a79de4d727aed2953d0e995d936b740905930a63
Zhao <bhkdos@gmail.com>
2026-03-08 15:45:23 +0800

    chore: CMake file & command

--- yolov5/cpp/fastrun.sh
+++ yolov5/cpp/fastrun.sh
@@ -4,4 +4,5 @@ set -e
 
 eval "~/Development/VisualStudioWorkspace/alubancat_yolo/yolov5/cpp/build-linux.sh -t rk3588"
 eval "cp ~/Desktop/yolov5s_relu.rknn ~/Development/VisualStudioWorkspace/alubancat_yolo/yolov5/cpp/install/rk3588_linux/"
+eval "cp ~/Desktop/world_params.csv ~/Development/VisualStudioWorkspace/alubancat_yolo/yolov5/cpp/install/rk3588_linux/"
 
