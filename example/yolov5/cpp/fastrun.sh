6869adf0962ec8899cce28798d1b9219bb082932
Zhao <bhkdos@gmail.com>
2026-03-08 15:48:53 +0800

    refactor: update treefile module

--- example/yolov5/cpp/fastrun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-eval "~/Development/VisualStudioWorkspace/alubancat_yolo/example/yolov5/cpp/build-linux.sh -t rk3588"
-eval "cp ~/Desktop/yolov5s_relu.rknn ~/Development/VisualStudioWorkspace/alubancat_yolo/example/yolov5/cpp/install/rk3588_linux/"
-
