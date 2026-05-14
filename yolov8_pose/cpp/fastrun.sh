5330e9cb0de1bb687bffa3d322a6536bf6431a6e
Zhao <bhkdos@gmail.com>
2026-05-14 14:06:26 +0800

    build: add script to build CMakefile

---
+++ yolov8_pose/cpp/fastrun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+eval "~/Development/VisualStudioWorkspace/alubancat_yolo/yolov8_pose/cpp/build-linux.sh -t rk3588 -d"
+eval "cp ~/Development/VisualStudioWorkspace/alubancat_yolo/weights/yolov8_pose.rknn ~/Development/VisualStudioWorkspace/alubancat_yolo/yolov8_pose/cpp/install/rk3588_linux"
+
