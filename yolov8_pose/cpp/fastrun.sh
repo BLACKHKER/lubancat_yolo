4d8be927ace6132a9fbadb48b9054576a7c28aaa
Zhao <bhkdos@gmail.com>
2026-06-01 20:04:04 +0800

    chore: update CMake cp world_params csv & rknn model

--- yolov8_pose/cpp/fastrun.sh
+++ yolov8_pose/cpp/fastrun.sh
@@ -3,5 +3,5 @@
 set -e
 
 eval "~/Development/VisualStudioWorkspace/alubancat_yolo/yolov8_pose/cpp/build-linux.sh -t rk3588 -d"
-eval "cp ~/Development/VisualStudioWorkspace/alubancat_yolo/weights/yolov8_pose.rknn ~/Development/VisualStudioWorkspace/alubancat_yolo/yolov8_pose/cpp/install/rk3588_linux"
+# eval "cp ~/Development/VisualStudioWorkspace/alubancat_yolo/weights/yolov8_pose.rknn ~/Development/VisualStudioWorkspace/alubancat_yolo/yolov8_pose/cpp/install/rk3588_linux"
 
